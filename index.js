c3993077f7346a85a4940f7a97efb3908e8b0c73
mkpatel-247 <meet.codes247@gmail.com>
2024-09-18 00:22:36 +0530

    Created node server and routes.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Starting with node backend");
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/angular', (req, res) => {
+    res.send('<h1>Angular is powerful frontend framework.</h1>')
+})
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port: ${process.env.PORT}`)
+})
