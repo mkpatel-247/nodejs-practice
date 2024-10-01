5d99d1cdb71f9b8c34c067c610d0842190454cc0
mkpatel-247 <meet.codes247@gmail.com>
2024-10-02 01:00:32 +0530

    Practice basic read and write operations.

--- practice-tasks/index.js
+++ practice-tasks/index.js
@@ -1,12 +1,29 @@
 const http = require("http");
 const fs = require("fs");
-const router = require("router");
+const router = require("router")();
+const bodyParser = require("body-parser");
+const port = 9000;
+
+
+router.use(bodyParser.json());
 
 http.createServer((request, response) => {
-    router.use(req, res, () => {
-
-    })
-}).listen(9000, () => {
+    router(request, response, () => {
+        // console.log(res.headers)
+    });
+}).listen(port, () => {
     console.log(`Server Started at port number: ${port}`);
 });
 
+router.get('/', (req, res) => {
+    try {
+        const buf_data = fs.readFileSync('./data.json');
+        const dataJson = JSON.parse(buf_data.toString());
+        dataJson.push({ id: 5, name: "naman" }, { id: 6, name: "sunil1212" });
+        fs.writeFileSync('./data.json', JSON.stringify(dataJson));
+        console.log(typeof dataJson);
+        return res.end("Got you request....");
+    } catch {
+        return res.end("Not working....");
+    }
+})
